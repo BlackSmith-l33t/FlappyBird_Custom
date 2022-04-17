63a60b82570669c2409b239e66535ed678f38f8b
DESKTOP-NK217EO\Melchizedek <nobody133738@gmail.com>
2022-04-18 00:00:19 +0900

    scrpt 추가

---
+++ Flappy Bird/Assets/Script/BackGround.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour
+{
+    [SerializeField] [Range(1f, 40f)] float speed = 3f;
+
+    [SerializeField] float posValue;
+
+    Vector2 startPos;
+    float newPos;
+    GameObject player;
+
+    private void Start()
+    {      
+        player = GameObject.Find("Player");
+        startPos = transform.position;
+    }
+
+    private void Update()
+    {
+        newPos = Mathf.Repeat(Time.time * speed, posValue);
+        transform.position = startPos + Vector2.left * newPos;
+
+        if (null == player)
+        {
+            GetComponent<BackGround>().enabled = false;
+        }
+    }
+}
+
+
